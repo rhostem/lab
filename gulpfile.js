4b2541cb18109bd40ccf54d5f18b38673027b9f4
jsyGit <syoung.j@gmail.com>
2016-01-29 01:39:05 +0900

    Flowing card grid : base algorithm is set

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
 	jade = require('gulp-jade'),
+	jshint = require('gulp-jshint'),
 	browserSync = require('browser-sync'),
 	reload      = browserSync.reload;
 
@@ -7,16 +8,28 @@ var config = {
 	'browserSync': { // http://www.browsersync.io/docs/options/
 		'server'  : [''],
 		'port'    : 0077,
-		'baseDir': "app",
+		// 'baseDir': "app",
     	'directory': true,
 		'notify'  : !true,
 		'browser' : 'chrome'
 		// 'browser' : 'firefox'
-	}
-}
+
+}}
 
 gulp.task('default', function () {
 	browserSync(config.browserSync);
+
+});
+
+gulp.task('js', function() {
+	browserSync(config.browserSync);
+
+	gulp.src("javascript/**/*.html")
+		.pipe( jshint() );
+
+	gulp.watch("javascript/**/*.html")
+		.on('change', browserSync.reload);
+
 });
 
 gulp.task('watch-jade', function() {
