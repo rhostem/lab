1c15a8ad4c3f62d26d39f3bd6a699a92e319f012
jsyoung <syoung.j@gmail.com>
2016-04-20 17:19:14 +0900

    new example(angello) added

--- gulp/sass.js
+++ gulp/sass.js
@@ -12,25 +12,24 @@ var filter = require('gulp-filter');
 var browserSync = require('browser-sync');
 
 global.gulpConfig.sass = {
-  errLogToConsole: true,
-  outputStyle: 'expanded'
+  process: {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+  },
+  autoprefixer: {
+    // https://github.com/ai/browserslist
+    browsers: ['last 2 versions', '> 1% in KR', 'ie 6-8'],
+    cascade: false
+  }
 }
 
 gulp.task('sass', function() {
  gulp.src( global.paths.sass.src )
     .pipe( sourcemaps.init() ) // init sourcemaps
-    // .pipe(
-    //   gulpif(
-    //     argv.all === undefined ,
-    //     changed( global.paths.sass.dist, { extension: '.css' })
-    //   )
-    // )
-    // .pipe( sass( global.gulpConfig.sass )
     .pipe( sass( global.gulpConfig.sass.process ).on('error', sass.logError) )
-    .pipe( autoprefixer({'browsers': ['last 2 versions']}) )
-    .pipe( sourcemaps.write('.') )
+    .pipe( autoprefixer(global.gulpConfig.sass.autoprefixer) )
+    .pipe( sourcemaps.write('./maps') )
     .pipe( gulp.dest( global.paths.sass.dist ) )
-    .pipe( filter('**/*.css') )
     .pipe( browserSync.reload({stream: true}) );
 });
 
