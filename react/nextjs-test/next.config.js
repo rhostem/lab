036ddb6b1e7f7b446181e1819a15a748e77a22c2
rhostem <syoung.j@gmail.com>
2017-07-07 14:49:51 +0900

    test static page generation of next.js 3.0 beta.
    https://zeit.co/blog/next3-preview

---
+++ react/nextjs-test/next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      "/": { page: "/" },
+      // "/p/learn-nextjs": { page: "/post", query: { title: "learn-nextjs" } }
+    };
+  },
+}
